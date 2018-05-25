f463e0b40caad8822490839f815b1f6f91cc3b73
B1u3Buf4 <450912640@qq.com>
2018-05-25 21:55:03 +0800

    添加总页数并优化间隔时间
    粗略估算爬取总页数；优化了等待时间，确保间隔时间不少于5秒。

--- NoDependence/xxoonodep.py
+++ NoDependence/xxoonodep.py
@@ -2,9 +2,10 @@
 '''
 Have fun.
 
+python 2
+
 @author: B1u3Buf4
 '''
-
 import hashlib
 import base64
 import requests
@@ -22,33 +23,45 @@ with open('record.txt', 'r') as f:
 
 def FirstReq():
     banner = 0
+    page = -1
     with open('allurls.txt', 'r') as log:
         allurls = list(set(log.readlines()))
     while banner < len(record):
+        page += 1
+        stime = time.time()
         r = requests.get(record[banner][:-1], headers=headers)
         for i in re.findall('jandan.net/ooxx/page-.*?#comments', r.text):
             if 'http://' + i + '\n' not in record:
                 record.append('http://' + i + '\n')
         total = re.findall('img-hash">.*?<', r.text)
-        print len(total)
+        try:
+            tmp = float(re.findall('\d*\.\d%',r.text)[0][:-1])
+            tmp = int((page * 100)/tmp)
+        except:
+            tmp = 0
+        if tmp == 0 and page > 0:
+            banner += 1
+            page -= 1
+            continue
+        print len(total), page, '/', tmp
         for j in total:
             tmp = base64.b64decode(j[10:-1])
             tmp = re.sub('cn/.*?/','cn/large/',tmp)
             tmp = 'http:' + tmp
             if tmp + '\n' not in allurls:
-                print(tmp)
+                print '[+]',tmp
                 rr = requests.get(tmp, headers = headers)
                 with open('./pics/' + tmp[tmp.rfind('/') + 1:], 'wb') as f:
                     f.write(rr.content)
                 with open('allurls.txt', 'a') as log:
                     log.write(tmp + '\n')
         banner += 1
-        time.sleep(5)
+        sptime = time.time() - stime
+        if sptime < 5:
+            time.sleep(5 - sptime)
 
 if __name__ == '__main__':
     if not os.path.exists('./pics/'):
         os.mkdir('pics')
     if len(sys.argv) == 1:
         FirstReq()
-    #if len(sys.argv) == 3:
-    #    GetHash(sys.argv[1],sys.argv[2])
